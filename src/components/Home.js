30103a31947df5c9f19eac82530eaa52c8cb54c3
ClientHenry <wanghao0628@hotmail.com>
2024-06-22 22:17:50 +1200

    Home Page

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,14 @@ import React from 'react';
 function Home(props) {
     return (
         <div>
-            <p>Welcome</p>
+            <p>Admin Account: admin@gmail.com</p>
+            <p>Admin Password: 123456</p>
+            <p>Lecturer Account: lecturer@gmail.com</p>
+            <p>User Password: 123456</p>
+            <p>Student Account: student@gmail.com</p>
+            <p>Student Password: 123456</p>
+            <p>Registration Rules: User name is the email, password is the birthday</p>
+            <p>Example: User name: henry@gmail.com, password: 1991-02-08 (note the format of birthday)</p>
         </div>
     );
 }
